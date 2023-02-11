0c14b44c9b7c14adf678850692eab2c8d0424b75
e0ty98r9u78ry90u <110433702+e0ty98r9u78ry90u@users.noreply.github.com>
2023-02-11 10:36:06 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,8 @@
 local player = game.Players.LocalPlayer
 
 --Speed (can use normal speed 💀)
+while true do
+  player.Character.Humanoid.WalkSpeed = 23 -- Sets To Max Speed
+  wait(0.5) --Gives you time with normal speed
+  player.Character.Humanoid.WalkSpeed = 17 --Sets to 17 speed to prevent flags
+end
